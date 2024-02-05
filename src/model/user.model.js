6d948ed758dae86724972e3b73fb1141689af0bf
Punit <3033.punit@gmail.com>
2024-02-06 02:37:55 +0530

    feat: project connected to mongodb database

---
+++ src/model/user.model.js
@@ -0,0 +1,31 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, //when the field is extensively used in searching
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+export const User = model("User", userSchema);
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -7,4 +7,4 @@ const router = Router();
 
 router.route("/register").post(register);
 
-export default router;
+export {router};
